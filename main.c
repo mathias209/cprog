839f2c4bc0697b840af4f28e78dff2014fb9413c
mathias209 <s255552@dtu.dk>
2025-09-03 15:13:11 +0200

    master

--- main.c
+++ main.c
@@ -1,23 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAXLINE 1000
-
-void getlinesimple(char s[]);
-
 int main()
 {
     int motors = 0;
     float weight = 0.0;
 
     printf("How many motors are carrying the packages?: ");
-    char buffer[MAXLINE];
-    getlinesimple(buffer);
-    motors = atoi(buffer);
+    scanf("%d", &motors);
 
     printf("How many kg of packages do we expect?: ");
-    getlinesimple(buffer);
-    weight = atof(buffer);
+    scanf("%g", &weight);
 
     if (weight / motors <= 5.6)
 	printf("Yes! The conveyor belt can carry the packages.\n");
@@ -25,15 +18,4 @@ int main()
 	printf("No. The conveyor belt cannot carry the packages.\n");
 
     return 0;
-}
-
-void getlinesimple(char s[])
-{
-    int i = 0;
-    char c;
-
-    while ((c = getchar()) != '\n')
-	s[i++] = c;
-
-    s[i] = '\0';
 }
