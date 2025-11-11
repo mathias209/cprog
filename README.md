d1e3ae8ecb33bbfb7edb84f878378fe02e722708
mathias209 <s255552@dtu.dk>
2025-11-11 15:26:28 +0100

    spareparts - med makefile

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # cprog
-Flowchart og program for conveyrbelt lavet af Mathias.
+Flowchart og program for spare parts shop lavet af Mathias.
 
-Programmet tager input fra brugeren for at finde ud af hvor mange motorer og hvor meget vægt de skal bærer. Derefter bliver der tjekket om der er nok motorer til at bærer vægten.
+Programmet har en liste af parts og en liste af deres stock. Der kører så et uendeligt loop som spørger efter en prompt fra brugeren. Hvis brugeren køber noget bliver der trykket 1 fra dens stock. Desuden er der en help og liste kommando.
 
 Uge 2
--- main.c
+++ main.c
@@ -1,20 +1,55 @@
 #include <stdio.h>
+#include <string.h>
+
+#define MAXLINE 1000
+
+void simpleGetline(char s[], int maxlen);
 
 int main()
 {
-    int motors = 0;
-    float weight = 0.0;
+    int nParts = 3;
+    char *parts[] = { "hydraulic pump", "PLC module", "servo motor" };
+    int stock[] = { 5, 5, 5 };
 
-    printf("How many motors are carrying the packages?: ");
-    scanf("%d", &motors);
+    char prompt[MAXLINE];
 
-    printf("How many kg of packages do we expect?: ");
-    scanf("%g", &weight);
 
-    if (weight / motors <= 5.6)
-	printf("Yes! The conveyor belt can carry the packages.\n");
-    else
-	printf("No. The conveyor belt cannot carry the packages.\n");
+    printf("Hej. Welcome to the spare parts inventory! (type \"help\" for help)\n");
+    while (1) {
+	printf("prompt> ");
+	simpleGetline(prompt, MAXLINE);
+	int part = -1;
+	for (int i = 0; i<nParts; i++)
+	    if (strcmp(prompt, parts[i]) == 0) {
+		part = i;
+		break;
+	    }
+	if (part != -1 && stock[part] > 0) {
+	    printf("I have got %s here for you :). Bye!\n", parts[part]);
+	    stock[part]--;
+	} else if (part != -1) {
+	    printf("Sorry, %s is out of stock :(\n", parts[part]);
+	} else if (strcmp(prompt, "list") == 0) {
+	    for (int i = 0; i<nParts; i++)
+		printf("%s: %d\n", parts[i], stock[i]);
+	} else if (strcmp(prompt, "help") == 0) {
+	    printf("Type name of part to buy\nType \"list\" for a list of parts and their stock\nType \"help\" for this help message\nType \"quit\" to exit program\n");
+	} else if (strcmp(prompt, "quit") == 0) {
+	    break;
+	} else {
+	    printf("ERROR: Invalid prompt: %s. Type \"help\" for help\n", prompt);
+	}
+    }
 
     return 0;
+}
+
+void simpleGetline(char s[], int maxlen)
+{
+    int i = 0;
+    char c;
+
+    while ((c = getchar()) != '\n')
+	s[i++] = c;
+    s[i] = '\0';
 }
